2061109db50cf27d934c3bb0f57f5de57adf5a85
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-05-09 20:21:41 +0530

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Button = () => {
+  return (
+    <button className="btn-complex">
+      <div className="btn-inner-complex text"></div>
+    </button>
+  );
+};
+
+export default Button;
