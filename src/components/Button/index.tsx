6547e3a6b9b4bf1bdc478c2a8479d4898cd961a1
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-05-11 18:47:13 +0530

    change button attributes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,18 @@
 import "./styles.scss";
 
-const Button = () => {
+type Props = {
+  bfrContent: string;
+  aftrContent: string;
+};
+
+const Button = ({ bfrContent, aftrContent }: Props) => {
   return (
     <button className="btn-complex">
-      <div className="btn-inner-complex text"></div>
+      <div
+        className="btn-inner-complex text"
+        bfr-content={bfrContent}
+        aftr-content={aftrContent}
+      ></div>
     </button>
   );
 };
