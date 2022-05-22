64d897624856b624fc3fc8d1b0baa3c1a1624f24
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-05-22 12:43:57 +0530

    extract about scroll to comp

---
+++ src/components/AboutScroll/AboutScrollAtom.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from 'react';
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
+
+type Props = {
+  offset: number;
+};
+
+function AboutScrollAtom({ offset }: Props) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useViewportScroll();
+  let top = 0;
+
+  const x = useTransform(
+    scrollYProgress,
+    [0, 0.4 + offset * 0.03],
+    [top + offset * 200, 0],
+  );
+
+  useEffect(() => {
+    const rect = ref.current?.getBoundingClientRect();
+    top = rect!.top;
+  }, [ref]);
+
+  return (
+    <section ref={ref}>
+      <motion.aside
+        style={{
+          x,
+        }}
+      >
+        Customer Research
+      </motion.aside>
+    </section>
+  );
+}
+
+export default AboutScrollAtom;
