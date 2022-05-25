9b54e7547399a1319b2111ebddf43a6f3f29a257
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-05-25 19:37:43 +0530

    style contact form

--- src/components/Contact/Form.tsx
+++ src/components/Contact/Form.tsx
@@ -1,41 +1,52 @@
+import Button from '../Button';
+import './styles.scss';
+
 const FormElements = () => {
   return (
-    <div className="form-wrapper">
+    <form className="form-wrapper">
       <div className="form-row">
         <div className="row-element">
-          <label className="label-input">First name</label>
           <input type="text" className="form-input" />
+          <label className="label-input">First name *</label>
         </div>
         <div className="row-element">
+          <input type="text" className="form-input" />
           <label className="label-input">Last name</label>
-          <input type="text" className="form-input" />
         </div>
       </div>
       <div className="form-row">
         <div className="row-element">
-          <label className="label-input">Email</label>
           <input type="email" className="form-input" />
+          <label className="label-input">Email *</label>
         </div>
 
         <div className="row-element">
-          <label className="label-input">Phone</label>
           <input type="text" className="form-input" />
+          <label className="label-input">Phone</label>
         </div>
       </div>
 
       <div className="form-row">
-        <div className="row-element full-row">
-          <label className="label-input">Message</label>
+        <div className="row-element">
           <textarea
             name="message"
             id="msg"
             cols={30}
-            rows={10}
+            rows={8}
             className="msg-box"
           ></textarea>
+          <label className="label-input">Message *</label>
         </div>
       </div>
-    </div>
+
+      <div className="btn-wrapper">
+        <Button
+          bfrContent="Submit"
+          aftrContent="Drop a message"
+          buttonOuterWidth="90px"
+        />
+      </div>
+    </form>
   );
 };
 
