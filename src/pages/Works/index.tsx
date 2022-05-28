17fa3b887444ceb1ffd41822bc0a50008c5ef3a5
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-05-28 19:31:59 +0530

    add filter to works

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Header } from '../../components';
 import withFooter from '../../hoc/footerHOC';
@@ -8,9 +8,100 @@ import desertImg from '../../../assets/desertImg.jpg';
 
 import './styles.scss';
 
+const data = [
+  [
+    {
+      imageSrc: desertImg,
+      categories: ['Art', 'Portrait', 'Creative direction'],
+    },
+    {
+      imageSrc: desertImg,
+      categories: ['Art', 'Group direction', 'Brand featuring'],
+    },
+  ],
+  [
+    {
+      imageSrc: desertImg,
+      categories: ['Group direction', 'Portrait', 'Creative direction'],
+    },
+    {
+      imageSrc: desertImg,
+      categories: ['Art', 'Creative direction', 'Portrait'],
+    },
+  ],
+  [
+    {
+      imageSrc: desertImg,
+      categories: ['Art', 'Brand featuring', 'Portrait', 'Group direction'],
+    },
+    {
+      imageSrc: desertImg,
+      categories: ['Art', 'Brand featuring', 'Creative direction'],
+    },
+  ],
+  [
+    {
+      imageSrc: desertImg,
+      categories: [
+        'Art',
+        'Brand featuring',
+        'Group direction',
+        'Creative direction',
+      ],
+    },
+    {
+      imageSrc: desertImg,
+      categories: ['Brand featuring', 'Group direction', 'Creative direction'],
+    },
+  ],
+];
+
+type FilteredDataType = {
+  imageSrc: string;
+  categories: string[];
+}[][];
+
 const Work = () => {
   const [selectedFilter, setSelectedFilter] = useState('All work');
   const [isFilterOpen, setFilterOpen] = useState(false);
+  const [filteredWorks, setFilteredWorks] = useState(data);
+
+  useEffect(() => {
+    const getFilteredWork = () => {
+      if (selectedFilter === 'All work') {
+        setFilteredWorks(data);
+        return;
+      }
+
+      const filteredData: FilteredDataType = [];
+
+      data.forEach((batch) => {
+        batch.forEach((work) => {
+          if (work.categories.includes(selectedFilter)) {
+            if (
+              filteredData.length &&
+              filteredData[filteredData.length - 1].length < 2
+            ) {
+              filteredData[filteredData.length - 1].push(work);
+            } else {
+              filteredData.push([work]);
+            }
+          }
+        });
+      });
+      setFilteredWorks(filteredData);
+    };
+
+    getFilteredWork();
+  }, [selectedFilter]);
+
+  const allOptions = [
+    'Art',
+    'Brand featuring',
+    'Group direction',
+    'Portrait',
+    'Creative direction',
+  ];
 
   return (
     <motion.div
@@ -39,48 +130,75 @@ const Work = () => {
           className="selected-filter-container"
           onClick={() => setFilterOpen(!isFilterOpen)}
         >
-          <div className="hvr-underln-anim selected-filter-container-inner">
-            <span className="work-category">{selectedFilter}</span>
+          <div className="selected-filter-container-inner">
+            <span className="work-category">
+              <span className="hvr-underln-anim">{selectedFilter}</span>
+            </span>
             <span className="work-plus">+</span>
           </div>
           <div
             className="filter-list"
             style={!isFilterOpen ? { display: 'none' } : {}}
           >
-            {['Art', 'Design', 'wildlife'].map((option) => {
-              return <div className="filter-option">{option}</div>;
-            })}
+            <>
+              <div
+                className="filter-option"
+                onClick={() => setSelectedFilter('All work')}
+              >
+                All work
+              </div>
+              {allOptions.map((option, i) => {
+                return (
+                  <div
+                    className="filter-option"
+                    onClick={() => setSelectedFilter(option)}
+                    key={i}
+                  >
+                    {option}
+                  </div>
+                );
+              })}
+            </>
           </div>
         </div>
       </div>
 
-      <div className="hori-separator"></div>
+      {filteredWorks.map((batch, i) => {
+        return (
+          <Fragment key={i}>
+            <div className="hori-separator"></div>
+            <div className="works-grid-container">
+              {batch.map((workSrc, index) => {
+                const batchClassName =
+                  index % 2 === 0 ? 'work-first-grid' : 'work-second-grid';
 
-      <div className="works-grid-container">
-        <div className="works-image-container work-first-grid">
-          <img src={desertImg} alt="" className="works-grid-image" />
-          <div className="categories">Strategies . Brand Identity . Art</div>
-        </div>
-
-        <div className="works-image-container work-second-grid">
-          <img src={desertImg} alt="" className="works-grid-image" />
-        </div>
-      </div>
-
-      <div className="hori-separator"></div>
-      <div className="works-grid-container">
-        <div className="works-image-container work-first-grid">
-          <img src={desertImg} alt="" className="works-grid-image" />
-          <div className="categories">
-            Strategies <span className="center-dot">.</span> Brand Identity .
-            Art
-          </div>
-        </div>
-
-        <div className="works-image-container work-second-grid">
-          <img src={desertImg} alt="" className="works-grid-image" />
-        </div>
-      </div>
+                return (
+                  <div
+                    className={`works-image-container ${batchClassName}`}
+                    key={index}
+                  >
+                    <img
+                      src={workSrc.imageSrc}
+                      alt={`${workSrc.imageSrc}-img`}
+                      className="works-grid-image"
+                    />
+                    <div className="categories">
+                      {workSrc.categories.map((category, i) => (
+                        <span className="category" key={i}>
+                          {category}{' '}
+                          {workSrc.categories.length - 1 !== i && (
+                            <span className="center-dot">.</span>
+                          )}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </Fragment>
+        );
+      })}
     </motion.div>
   );
 };
